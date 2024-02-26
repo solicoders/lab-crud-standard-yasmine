3f44eacaa1fe972b0c712ab6291d8cdfaf7ccc03
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-02-26 13:36:29 +0100

    add maquetee

---
+++ maquette/view/layouts/aside.php
@@ -0,0 +1,43 @@
+<aside class="main-sidebar sidebar-dark-info elevation-4 position-fixed">
+  <!-- Logo de la marque -->
+  <a href="../gestion_des_projet/projets/index.php" class="brand-link">
+    <img src="../assets/images/logo.png" class="brand-image img-circle elevation-3" alt="Image de groupe">
+    <span class="brand-text font-weight-light text-center">Gestion de Projets</span>
+  </a>
+
+  <!-- Barre latérale -->
+  <div class="sidebar">
+    <!-- Menu latéral -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="../projets/index.php" class="nav-link active">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Projets
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../taches/index.php" class="nav-link ">
+              <i class="nav-icon fas fa-tasks"></i>
+              <p>
+                Tâches
+              </p>
+            </a>
+          </li>
+          <!-- MEMBRE -->
+          <li class="nav-item">
+            <a href="../utilisateurs/index.php" class="nav-link ">
+              <i class="fa-solid fa-users pl-1 pr-1"></i>
+              <p>
+                Utilisateur
+              </p>
+            </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
