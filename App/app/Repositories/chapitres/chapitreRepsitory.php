63dfcfeb3f9e3ddbb557074cacc53929978e7d4b
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-04 16:39:59 +0100

    update

---
+++ App/app/Repositories/chapitres/chapitreRepsitory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\chapitres;
+
+use App\Models\chapitres\Chapitre;
+use App\Models\chapitres\Chapitres;
+use App\Repositories\BaseRepository;
+use Database\Seeders\chapitres\chapitresSeeders;
+use Illuminate\Database\Eloquent\Model;
+
+class  ChapitreRepository extends BaseRepository {
+    protected $model;
+
+ function __construct(Chapitre $Chapitre) {
+    $this->model = $Chapitre;
+}
+  
+public function searchData($searchdata , $perpage=4){
+ 
+  return $this ->model->where(function($query)use($searchdata){
+
+    $query->where('nom','like','%'.$searchdata.'%');
+
+  }) ->paginate($perpage);
+
+}
+}
