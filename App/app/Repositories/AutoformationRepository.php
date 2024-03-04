414ea25eed4dccf818e4fc308778136b7a5fc115
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-04 10:55:22 +0100

    update

---
+++ App/app/Repositories/AutoformationRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Autofromations;
+
+use App\Models\AutoFormation;
+use App\Repository\Repositories\AppBaseRepository;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjetRepository extends AppBaseRepository {
+    protected $model;
+
+  
+}
