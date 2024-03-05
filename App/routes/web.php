68b4204b07e4334fde26edda6080adc98e485416
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 15:41:04 +0100

    update

--- App/routes/web.php
+++ App/routes/web.php
@@ -37,32 +37,32 @@ Route::get('/test/store', function () {
     }
 });
 
-// Route::get('/test/update', function () {
-//     $repository = new ChapitreRepository(new Chapitre);
-//     $validatedata = [
-//         'name' => 'yasmine' ,
-//         'description' => 'nonoooooooooooo', 
-//         'date_debut' => '2024-03-04 13:11:49',
-//         'date_de_fin' => '2024-03-04 13:11:49',
-            //  'autoformationId' => 2
-//     ];
+Route::get('/test/update', function () {
+    $repository = new ChapitreRepository(new Chapitre);
+    $validatedata = [
+        'name' => 'yasmine' ,
+        'description' => 'nonoooooooooooo', 
+        'date_debut' => '2024-03-04 13:11:49',
+        'date_de_fin' => '2024-03-04 13:11:49',
+             'autoformationId' => 2
+    ];
 
-//     $insert = $repository->update($validatedata , 1);
-//     if($insert){
-//         echo 'update réussie';
-//     }else{
-//         echo 'Insertion n\'est pas réussite';
-//     }
-// });
+    $insert = $repository->update($validatedata , 1);
+    if($insert){
+        echo 'update réussie';
+    }else{
+        echo 'Insertion n\'est pas réussite';
+    }
+});
 
-// Route::get('/test/{id}', function ($id) {
-//     $repository = new ChapitreRepository(new Chapitre);
-//     $delete = $repository->destroy($id);
-//     if($delete){
-//         echo 'delete réussie';
-//     }else{
-//         echo 'delete n\'est pas réussite';
-//     }
-// });
+Route::get('/test/{id}', function ($id) {
+    $repository = new ChapitreRepository(new Chapitre);
+    $delete = $repository->destroy($id);
+    if($delete){
+        echo 'delete réussie';
+    }else{
+        echo 'delete n\'est pas réussite';
+    }
+});
 
 
