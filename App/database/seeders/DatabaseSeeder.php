aa53e878b13304ef3a90f19c513a26340dc00ea6
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 13:35:00 +0100

    Revert "push"
    This reverts commit 02b1479733a338914d2b31a66cddd0aa714bf91f.

---
+++ App/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\autoformations\AutoformationsSeeder;
+use Illuminate\Database\Seeder;
+
+use Database\Seeders\GestionChapitres\{
+    chapitresSeeder,
+};
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+   
+        public function run(): void
+        {
+            $classes = [];
+            $classes = array_merge(
+                GestionChapitres::Classes()
+            );
+            $this->call($classes);
+    
+    
+           
+        }
+    
+}
