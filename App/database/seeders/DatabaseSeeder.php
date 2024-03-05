d43f414d37828deb551b2c2802b6193458184975
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 13:05:23 +0100

    uodate

--- App/database/seeders/DatabaseSeeder.php
+++ App/database/seeders/DatabaseSeeder.php
@@ -5,18 +5,26 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Database\Seeders\GestionChapitres\{
+    chapitresSeeder,
+};
+
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+   
+        public function run(): void
+        {
+            $classes = [];
+            $classes = array_merge(
+                GestionChapitres::Classes()
+            );
+            $this->call($classes);
+    
+    
+           
+        }
+    
 }
