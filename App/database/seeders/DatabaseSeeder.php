44fe095e43845e00c4484b39a0923ef38ec40206
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 15:10:36 +0100

    update

--- App/database/seeders/DatabaseSeeder.php
+++ App/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\chapitres;
 use Database\Seeders\autoFormations\{
     AutoformationsSeeder,ChapitresSeeder
 };
