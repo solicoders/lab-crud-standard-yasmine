17683f6ac7da8cdc68624892bc3994a0fa71f547
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 14:06:13 +0100

    update

--- App/database/seeders/DatabaseSeeder.php
+++ App/database/seeders/DatabaseSeeder.php
@@ -5,8 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use Database\Seeders\GestionChapitres\{
-    chapitresSeeder,
+use Database\Seeders\GestionAutoFormations\{
+    AutoformationsSeeder,
 };
 
 class DatabaseSeeder extends Seeder
