cdd97bf44aa25fc458382c5e589e8e9b5d862b4a
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 15:14:06 +0100

    update

---
+++ App/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Database\Seeders\autoFormations\{
+    AutoformationsSeeder,ChapitresSeeder
+};
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+   
+        public function run(): void
+        {
+            $classes = [];
+            $classes = array_merge(
+                GestionAutoFormations::Classes(),
+                GestionChapitres::Classes()
+            );
+            $this->call($classes);
+    
+    
+           
+        }
+    
+}
