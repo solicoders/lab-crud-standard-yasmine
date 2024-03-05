93e39286724b279cd53a8312b7b38fea179af1c2
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-05 13:39:04 +0100

    update

--- App/database/seeders/DatabaseSeeder.php
+++ App/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Database\Seeders\autoformations\AutoformationsSeeder;
 use Illuminate\Database\Seeder;
 
 use Database\Seeders\GestionChapitres\{
