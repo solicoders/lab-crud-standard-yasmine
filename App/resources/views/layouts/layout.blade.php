c1929e8f1a0584b954e3eeb3ff99a68f51ce1d73
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-06 11:12:56 +0100

    add  views

---
+++ App/resources/views/layouts/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+
+    <!-- TinyMCE -->
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+</head>
+
+<body class="sidebar-mini" >
+<div class="wrraper">
+    @include('layouts.nav')
+    @include('layouts.aside')
+
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+</div>
+  
+    @include('layouts.footer')
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    
+</body>
+
+</html>
