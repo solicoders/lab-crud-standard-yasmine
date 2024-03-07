02a79a5e764cdce268b3bb693f3bd0a4de785f63
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2024-03-07 11:09:56 +0100

    update

--- App/routes/chapitres/chapitres.php
+++ App/routes/chapitres/chapitres.php
@@ -8,5 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::resource('chapitres' , ChapitreController::class);
-Route::get('export', [ChapitreController::class, 'export'])->name('chapitres.export');
-Route::post('import', [ChapitreController::class, 'import'])->name('chapitres.import');
+
+
+Route::get('chapitres/show/{id}', [ChapitreController::class, 'show'])->name('chapitres.show');
